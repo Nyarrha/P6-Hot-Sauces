677a32d1561eead4ce222c9f7614fb0f28d4d823
Nyarrha <paradisquentin@yahoo.fr>
2023-06-01 17:04:30 +0200

    Correction erreurs like/début sécurisation

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -1,73 +1,47 @@
-// Importation modèle sauce
+    // Importation modèle sauce
 const Sauce = require('../models/sauce');
 
+    // Exports entre route et middleware
 exports.likeSauce = (req, res, next) => {
+      // Trouve la sauce en comparant _id à l'id de la sauce demandée
     Sauce.findOne({ _id: req.params.id})
       .then((sauce) => {
-        // Afficher objet trouvé
-        console.log('Résultat objet trouvé :');
-        console.log(sauce);
-              // Like = 1 (likes += 1)
-        // Si userId n'est pas dans la liste usersLiked et que le user likes = 1
-        if(!sauce.usersLiked.includes(req.body.userId) && req.body.likes === 1) {
-          Sauce.updateOne(
-            { _id: req.params.id},
-            {
-              $inc: {likes: 1},
-              $push: {usersLiked: req.body.userId}
+        const userId = req.auth.userId;
+          // Traitement différents cas like(selon req user)
+        switch (req.body.like) {
+           // Cas : user like +1, vérifier qu'il n'est pas dans les array liked/disliked puis l'ajouter à array liked
+          case 1:
+            if(!sauce.usersLiked.includes(userId) && !sauce.usersDisliked.includes(userId)) {
+              sauce.usersLiked.push(userId);
             }
-          )
-          .then(() => res.status(201).json({ message: 'Sauce like +1'}))
-          .catch((error) => res.status(400).json(error));
-        };
-
-                // Like = 0
-        // Si userId est dans usersLiked et user likes = 0
-        if(sauce.usersLiked.includes(req.body.userId) && req.body.likes === 0) {
-          Sauce.updateOne(
-            { _id: req.params.id},
-            {
-              $inc: {likes: -1},
-              $pull: {usersLiked: req.body.userId}
+            break;
+            // Cas : user like 0, vérifier dans quel tableau(liked/disliked) user est puis l'en retirer
+          case 0:
+            if(sauce.usersLiked.includes(userId)) {
+              const indexUser = sauce.usersLiked.indexOf(userId);
+              sauce.usersLiked.splice(indexUser, 1);
+            };
+            if(sauce.usersDisliked.includes(userId)) {
+              const indexUser = sauce.usersDisliked.indexOf(userId);
+              sauce.usersDisliked.splice(indexUser, 1);
+            };
+            break; 
+            // Cas : user like -1, vérifier qu'il n'est pas dans les array liked/disliked puis l'ajouter à array disliked
+          case -1:
+            if(!sauce.usersLiked.includes(userId) && !sauce.usersDisliked.includes(userId)) {
+              sauce.usersDisliked.push(userId);
             }
-          )
-          .then(() => res.status(201).json({ message: 'Sauce neutre 0'}))
-          .catch((error) => res.status(400).json(error));
-        };
-
-                // Dislike = 1 (dislikes += 1)
-        // Si userId n'est pas dans usersDisliked et user dislikes = 1
-        if(!sauce.usersDisliked.includes(req.body.userId) && req.body.likes === -1) {
-          Sauce.updateOne(
-            { _id: req.params.id},
-            {
-              $inc: {dislikes: 1},
-              $push: {usersDisliked: req.body.userId}
-            }
-          )
-          .then(() => res.status(201).json({ message: 'Sauce dislike -1'}))
-          .catch((error) => res.status(400).json(error));
-        };
-
-                // Dislike = 0
-        // Si userId est dans usersDisliked et user dislikes = 0
-        if(sauce.usersDisliked.includes(req.body.userId) && req.body.likes === 0) {
-          Sauce.updateOne(
-            { _id: req.params.id},
-            {
-              $inc: {dislikes: -1},
-              $pull: {usersDisliked: req.body.userId}
-            }
-          )
-          .then(() => res.status(201).json({ message: 'Sauce neutre 0'}))
-          .catch((error) => res.status(400).json(error));
-        };
+            break;
+          default:
+            res.status(400).json('Bad request');
+            break;
+        }
+          // Affichage nombre de likes et dislikes puis save pour mettre à jour
+        sauce.usersLiked.likes = sauce.usersLiked.length;
+        sauce.usersDisliked.likes = sauce.usersDisliked.length;
+        sauce.save()
+        .then(() => res.status(200).json({message: "Sauce notée"}))
+        .catch((error) => res.status(401).json(error));
       })
       .catch((error) => res.status(404).json(error));
     };
-
-
-    // *********        ******* //
-    // Bug noté : en cas de requête Postman, si key "dislikes" = 1, ajoute quand même le userId dans usersLiked en doublons
-    // Régler création doublons
-    // Régler souci auth(userId et token non valides, mais ça envoie quand même la requête)
