8cd5856b1c0774ff74c2413d2c1c3f3a8f92edba
Nyarrha <paradisquentin@yahoo.fr>
2023-06-09 13:12:56 +0200

    fin commentaires server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+// Importation modules
 const http = require('http');
 require('dotenv').config()
 const app = require('./app');
 
+// Renvoie un port valide sous forme de numéro ou de chaîne
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -16,6 +18,8 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
+
+// Gère les différents cas d'erreurs avant de les enregistrer dans le serveur
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -39,6 +43,7 @@ const errorHandler = error => {
 const server = http.createServer(app);
 
 server.on('error', errorHandler);
+// Ecoute l'évènement du lancement de serveur pour consigner le port sur lequel il est lancé
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
